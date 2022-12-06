e77c20c0fd1528c0529c031354989a2c5a840720
milindsahu <62508132+milindsahu@users.noreply.github.com>
2022-12-06 15:07:15 +0530

    Update accuknox_restaurant.py

--- accuknox_restaurant.py
+++ accuknox_restaurant.py
@@ -1,13 +1,17 @@
 import collections
 
-a = [(1,10),(2,20),(3,30),(4,10),(6,10),(7,20),(6,10) ]
+# here in a, (1,10) 1 is the eater_id, and 10 is the foodmenu_id
+sample1 = [(1,10),(2,20),(3,30),(4,10),(6,10),(7,20),(6,10)]
+sample2 = [(4,18), (1,67),(5,90),(5,18),(1,67),(2,99)]
 
-print(collections.Counter(x[1] for x in a)) 
 
 x = 0
-while (x< len(a)):
-    if a.count(a[x])>1:
+while (x< len(sample1)):
+    if sample1.count(sample1[x])>1:
         print('Repetating Pair of Menu ID and Customer ID')
-        x = len(a)
+        x = len(sample1)
     else:
         x+=1
+      
+print(collections.Counter(x[1] for x in sample1)) 
+
