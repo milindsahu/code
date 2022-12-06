e57b1e98f5e2c509544f903542e68c609cd7c5fd
milindsahu <62508132+milindsahu@users.noreply.github.com>
2022-12-06 15:08:57 +0530

    Update accuknox_restaurant.py

--- accuknox_restaurant.py
+++ accuknox_restaurant.py
@@ -1,6 +1,7 @@
 import collections
 
-# here in a, (1,10) 1 is the eater_id, and 10 is the foodmenu_id
+# here in sample1, (1,10) 1 being the eater_id, and 10 being foodmenu_id 
+# sample1 is for error case where (6,10) appears twice, whereas sample2 is perfectly ideal case
 sample1 = [(1,10),(2,20),(3,30),(4,10),(6,10),(7,20),(6,10)]
 sample2 = [(4,18), (1,67),(5,90),(5,18),(1,67),(2,99)]
 
